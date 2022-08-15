d96449e421698d5e2ab1aabbff4c340d4a75fd9a
s-hirose <shunichi_hirose@pj100.biz>
2022-08-15 15:54:06 +0900

    :bug: nginxとdjangoが連携しない問題を修正

--- djangopj/settings.py
+++ djangopj/settings.py
@@ -17,17 +17,16 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# .envファイルから環境変数を読み込む
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7_gxf2@i&vcgi0g1c56_z!^va^)mt3g*&-*a1l)4!ps*k6_g@6"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
@@ -126,7 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = "/static/"
 STATIC_URL = "/static/"
 
 # Default primary key field type
