5a347f8e47ee14f53a3c982f57b2f3157dee6f91
shun198 <shunhiroseluvmri@gmail.com>
2022-09-05 06:50:31 +0900

    :sparkles: swaggeruiの設定の追加

--- djangopj/settings.py
+++ djangopj/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "drf_spectacular",
 ]
 
@@ -134,3 +135,14 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "プロジェクト名",
+    "DESCRIPTION": "プロジェクトの詳細",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
